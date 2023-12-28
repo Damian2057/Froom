c6afd9348ddde27855f60f1387336bcb3de1d0dd
Damiaan <ddamian2057@gmail.com>
2023-12-28 21:18:33 +0100

    improving color detection in the image

--- ColorClassification/app/prediction/processing.py
+++ ColorClassification/app/prediction/processing.py
@@ -9,21 +9,33 @@ logger = logging.getLogger(__name__)
 TEMP_IMAGE_PATH = "TEMP_IMAGE_PATH"
 
 def predict_color(file):
-    k = 1
+    k = 2
     image_path = TEMP_IMAGE_PATH + datetime.now().strftime("%Y%m%d-%H%M%S") + ".jpg"
-    file.save(image_path)
-    logger.info("Image saved to %s", image_path)
 
-    # Load and preprocess the input image
-    img = Image.open(image_path)
-    img = img.resize((100, 100))  # Resize the image to speed up processing
-    img_array = np.array(img)
-    pixels = img_array.reshape((-1, 3))
-    kmeans = KMeans(n_clusters=k)
-    kmeans.fit(pixels)
-    main_color = kmeans.cluster_centers_.astype(int)[0]
+    try:
+        file.save(image_path)
+        logger.info("Image saved to %s", image_path)
 
-    os.remove(image_path)
-    logger.info("Image removed from %s", image_path)
+        img = Image.open(image_path)
+        width, height = img.size
 
-    return main_color.tolist()
+        roi_size = min(width, height) // 2
+        left = (width - roi_size) // 2
+        top = (height - roi_size) // 2
+        right = left + roi_size
+        bottom = top + roi_size
+
+        img = img.crop((left, top, right, bottom))
+        img = img.resize((100, 100))
+
+        img_array = np.array(img)
+        pixels = img_array.reshape((-1, 3))
+        kmeans = KMeans(n_clusters=k)
+        kmeans.fit(pixels)
+        dominant_color = kmeans.cluster_centers_.astype(int)[0]
+
+        return dominant_color.tolist()
+
+    finally:
+        os.remove(image_path)
+        logger.info("Image removed from %s", image_path)
